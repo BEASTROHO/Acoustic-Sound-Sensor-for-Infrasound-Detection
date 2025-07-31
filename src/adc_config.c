41ed50fc4fdee1052aaa9a093e19d258c60d595e
Rohit <hemamahendiran0@gmail.com>
2025-07-31 14:32:45 +0530

    Create adc_config.c

---
+++ src/adc_config.c
@@ -0,0 +1,38 @@
+#include "adc_config.h"
+#include "stm32f1xx_hal.h"
+
+ADC_HandleTypeDef hadc1;
+
+void ADC_Init(void) {
+    __HAL_RCC_ADC1_CLK_ENABLE();
+    __HAL_RCC_GPIOA_CLK_ENABLE();
+
+    GPIO_InitTypeDef GPIO_InitStruct = {0};
+    GPIO_InitStruct.Pin = GPIO_PIN_0;
+    GPIO_InitStruct.Mode = GPIO_MODE_ANALOG;
+    HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+    hadc1.Instance = ADC1;
+    hadc1.Init.ScanConvMode = ADC_SCAN_DISABLE;
+    hadc1.Init.ContinuousConvMode = DISABLE;
+    hadc1.Init.DiscontinuousConvMode = DISABLE;
+    hadc1.Init.DataAlign = ADC_DATAALIGN_RIGHT;
+    hadc1.Init.ExternalTrigConv = ADC_SOFTWARE_START;
+    HAL_ADC_Init(&hadc1);
+
+    ADC_ChannelConfTypeDef sConfig = {0};
+    sConfig.Channel = ADC_CHANNEL_0;
+    sConfig.Rank = ADC_REGULAR_RANK_1;
+    sConfig.SamplingTime = ADC_SAMPLETIME_71CYCLES_5;
+    HAL_ADC_ConfigChannel(&hadc1, &sConfig);
+}
+
+uint16_t Read_ADC(void) {
+    HAL_ADC_Start(&hadc1);
+    HAL_ADC_PollForConversion(&hadc1, HAL_MAX_DELAY);
+    return HAL_ADC_GetValue(&hadc1);
+}
+
+float Convert_To_Voltage(uint16_t adc_val) {
+    return (adc_val * 3.3f) / 4095.0f;
+}
